e75af8678c901febbd53a0f67f9805b8fb6667e3
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-05 23:12:06 +0530

    update 2

--- src/screens/ModernHomeScreen/index.tsx
+++ src/screens/ModernHomeScreen/index.tsx
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
 
-    zIndex: 1010, // 👈 BELOW EVERYTHING
+    zIndex: 1010, // 👈 BELOW EVERYTHINGert
     paddingHorizontal: 20,
     overflow: 'hidden',
     backgroundColor: 'red',
