5b9d217fdc68c66930e43981984ca87895df7000
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-07 23:22:55 +0530

    updatre

--- src/components/SearchableDropdown.tsx
+++ src/components/SearchableDropdown.tsx
@@ -13,7 +13,7 @@ import {
 
 interface SearchableDropdownProps {
   label: string;
-  value: string; // The selected name to display in the input
+  value: string; // The selected name to display in the input d
   onChangeText: (text: string) => void;
   data: any[];
   onSelectItem: (item: any) => void;
