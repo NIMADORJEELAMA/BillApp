d33dde89b4f1b85a83c39b1fd4fe6d5891c53080
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-11 23:29:42 +0530

    updatewe

--- src/components/Printer/LabelTemplate.tsx
+++ src/components/Printer/LabelTemplate.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     marginLeft: 5,
   },
   name: {
-    fontSize: 18, // Adjusted for physical 25mm height 123
+    fontSize: 18, // Adjusted for physical 25mm height
     fontWeight: '700',
     color: '#000',
   },
