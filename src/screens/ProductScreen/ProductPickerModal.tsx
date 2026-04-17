e2d2b6bcc727d90d757904122e3a98af06dbc0ef
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-17 23:25:38 +0530

    updatsdfg

--- src/screens/ProductScreen/ProductPickerModal.tsx
+++ src/screens/ProductScreen/ProductPickerModal.tsx
@@ -310,7 +310,7 @@ const styles = StyleSheet.create({
     fontSize: 13,
     fontWeight: '600',
     color: '#334155',
-    textAlign: 'center', // Left align looks more professional in gridsdg
+    textAlign: 'center', // Left align looks more professional in gridsdgertg
     lineHeight: 18,
     marginVertical: 4,
     height: 36, // Keep consistent height for grid alignment
