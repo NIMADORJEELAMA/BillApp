36051d54a32d0f1262988eeb0074cb4412d8b81a
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-18 23:32:38 +0530

    update modal

--- src/screens/ProductScreen/ProductPickerModal.tsx
+++ src/screens/ProductScreen/ProductPickerModal.tsx
@@ -294,7 +294,7 @@ const styles = StyleSheet.create({
     color: '#1e293b',
   },
   stockMiniBadge: {
-    // Elegant minimal stock indicator
+    // Elegant minimal stock indicator asdfg
     paddingHorizontal: 4,
     backgroundColor: '#6FCF97',
     borderRadius: 6,
