2f8fd1faea12212fcccf3717efe035077f97fbe0
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-23 23:11:42 +0530

    updateasfasf

--- src/screens/SalesScreen/SalesListScreen.tsx
+++ src/screens/SalesScreen/SalesListScreen.tsx
@@ -183,7 +183,7 @@ export default function SalesListScreen() {
     );
   }, [sales]);
 
-  // 5. Separate Render Item for Performancedfg
+  // 5. Separate Render Item for Performancedfgasdf
   const SaleItem = React.memo(({item, onPress}: any) => {
     const isCash = item.paymentMode === 'CASH';
     const date = new Date(item.createdAt);
