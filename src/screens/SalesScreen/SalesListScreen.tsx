c7408de38eabd17888993cb096ba73a099d162de
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-27 23:53:15 +0530

    update after breaks

--- src/screens/SalesScreen/SalesListScreen.tsx
+++ src/screens/SalesScreen/SalesListScreen.tsx
@@ -171,7 +171,7 @@ export default function SalesListScreen() {
       Toast.show({type: 'error', text1: 'Print failed. Check connection.'});
     }
   }, [selectedSale]);
-  ///asfdzdfg
+  ///asfdzdfgasdf
   const stats = useMemo(() => {
     return sales.reduce(
       (acc, curr) => {
