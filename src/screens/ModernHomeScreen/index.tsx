feab53906683457337d7e7263e5003663ca66fe8
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-04-29 23:33:23 +0530

    update 4

--- src/screens/ModernHomeScreen/index.tsx
+++ src/screens/ModernHomeScreen/index.tsx
@@ -77,7 +77,7 @@ const DASHBOARD_MENU = [
   //   color: '#FAF5FF',
   // },
 ];
-//asdfasdfadsg
+//asdfasdfadsgasdg
 const ModernHomeScreen = () => {
   const scrollY = useSharedValue(0);
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
