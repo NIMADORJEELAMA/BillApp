12169b5a753dbab1bdba3f41486fae3fab17d6a9
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-02 23:48:04 +0530

    update tab bar UI 2

--- src/components/common/AnimatedTabButton.tsx
+++ src/components/common/AnimatedTabButton.tsx
@@ -81,7 +81,7 @@ const AnimatedTabButton = (props: any) => {
     const borderAnimation = Animated.timing(borderWidth, {
       toValue: isSelected ? 100 : 0,
       duration: 300,
-      useNativeDriver: false, // Layout properties (width) can't use native driverasdfg
+      useNativeDriver: false, // Layout properties (width) can't use native driverasdfgasdf
     });
 
     const scaleAnimation = Animated.timing(scaleValue, {
