0c10c64a5fe5c48c9bce71e12e8e94af77a82c34
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-03 23:59:28 +0530

    update 6

--- src/screens/ProductScreen/BulkProductScreen.tsx
+++ src/screens/ProductScreen/BulkProductScreen.tsx
@@ -197,7 +197,7 @@ export default function BulkProductScreen() {
                   placeholder="Search or type new..."
                   value={searchQuery}
                   onChangeText={setSearchQuery}
-                  autofocus={true} // Fixed prop name from previous asdf
+                  autofocus={true} // Fixed prop name from previous asdfdfhasdf
                 />
               </View>
 
