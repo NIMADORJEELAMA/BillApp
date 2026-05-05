0cdbd81b3ed95a64c00597eac5d110c6f0444e07
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-05 23:44:00 +0530

    update 1

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -252,7 +252,7 @@ const CustomerModal = ({
                 />
                 {/* <GradientButton
                   title="Cancel"
-                  colors={['#F3F4F6', '#E5E7EB']} // Light gray gradient
+                  colors={['#F3F4F6', '#E5E7EB']} // Light gray gradientsfgrdg
                   onPress={() =>
                     initialData ? handleResetAndClose() : setViewMode('list')
                   }
