7e5ceec9753be52da1d427f4725fc8c30f7cc6e9
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-06 23:47:04 +0530

    updatre asdfgd

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -252,7 +252,7 @@ const CustomerModal = ({
                 />
                 {/* <GradientButton
                   title="Cancel"
-                  colors={['#F3F4F6', '#E5E7EB']} // Light gray dfhadfsgdfgfdghfghsddfgj dghsdfhfg
+                  colors={['#F3F4F6', '#E5E7EB']} // Light gray asdfgsadfgdfhkhj dghsdfhfg
                   onPress={() =>
                     initialData ? handleResetAndClose() : setViewMode('list')
                   }
