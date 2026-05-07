d8ba3783521b644b9779a9894961d327117d51dc
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-07 23:59:38 +0530

    updatea asdjfgskdfasdgasdftg

--- src/screens/SalesReportScreen/index.tsx
+++ src/screens/SalesReportScreen/index.tsx
@@ -87,7 +87,7 @@ const SalesReportScreen = () => {
         <ActivityIndicator size="large" color="#fa2c37" />
       </View>
     );
-  //asdgasdfgfdghsdfhgfdgh
+  //asdgasdfgfdghsdfhgfdghasdgsdfhgsdfh
   return (
     <MainLayout
       title="Sales Analytics"
