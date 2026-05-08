bf6276a207fb21d4a5f0760ab295b333ec26c98a
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-08 23:15:33 +0530

    chore: nothing sdfg

--- src/screens/SalesReportScreen/index.tsx
+++ src/screens/SalesReportScreen/index.tsx
@@ -87,7 +87,7 @@ const SalesReportScreen = () => {
         <ActivityIndicator size="large" color="#fa2c37" />
       </View>
     );
-  //fghjfghjfkl'kdasdfg
+  //fghjfghjfkl'kdasdfgfdghjfghj
   return (
     <MainLayout
       title="Sales Analytics"
