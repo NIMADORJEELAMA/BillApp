8eac93fecf73a567dcb8167b307f013e64863f0b
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-08 23:51:16 +0530

    chore: nothing uppp

--- src/screens/SalesReportScreen/index.tsx
+++ src/screens/SalesReportScreen/index.tsx
@@ -87,7 +87,7 @@ const SalesReportScreen = () => {
         <ActivityIndicator size="large" color="#fa2c37" />
       </View>
     );
-  //fghjfghjfkl'sdfhfghdfghj sdfhfghjkl'sdfhfghdfghj sdfhfghjkl'sdfhfghdfghj sdfhfghjkl'sdfhfghdfghj
+  //fghjfghjfkl'sdfhfghdfghj sdfhfghjkl'sdfhfghdfghj sdfhfghjkl'sdfhfghdfghj sdfhfghjkl'dfghdfghj
   return (
     <MainLayout
       title="Sales Analytics"
