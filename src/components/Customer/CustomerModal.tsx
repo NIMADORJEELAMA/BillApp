1e684fc8dc7e0ccfe29a5d75bedf6addcb6532b0
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-10 00:31:57 +0530

    update 12222545445

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -267,7 +267,7 @@ const CustomerModal = ({
                   containerStyle={styles.btnPrimary}
                 />
                 {/* <TouchableOpacity
-                  style={styles.saveBtn}  asdfasdgfdgfgh
+                  style={styles.saveBtn}   
                   onPress={handleSaveCustomer}
                   disabled={isSaving}>
                   {isSaving ? (
