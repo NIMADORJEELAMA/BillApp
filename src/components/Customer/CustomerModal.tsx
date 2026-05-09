f7c5213e200725c84c4d4c9980c3c814bed857e7
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-10 00:22:42 +0530

    update 21214356

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -257,7 +257,7 @@ const CustomerModal = ({
                     initialData ? handleResetAndClose() : setViewMode('list')
                   }
                   textStyle={{color: '#374151'}}  
-                  // disabled={isSubmitting} //  
+                  // disabled={isSubmitting} //  sdfgsdfg
                   containerStyle={styles.btnSecondary}  
                 /> */}
                 <GradientButton
