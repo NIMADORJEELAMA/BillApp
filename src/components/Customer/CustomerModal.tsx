61f42e3eb95e9356809e02eeaa47c852800c4bfe
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-09 23:49:40 +0530

    update 123

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -263,7 +263,7 @@ const CustomerModal = ({
                 <GradientButton
                   title={initialData ? 'Update' : 'Create & Select'}
                   onPress={handleSaveCustomer}
-                  loading={isSaving} // Show spinner when true sdfgsdfg sdfgsdfgsdf
+                  loading={isSaving} // Show spinner when true sdfgsdfg asdgfsfdgdf
                   containerStyle={styles.btnPrimary} // Keep your layout flex
                 />
                 {/* <TouchableOpacity
