140bc48f9ed4c84ddebe19fcb470e6cb65b87c54
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-16 23:37:55 +0530

    update all after travel

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -279,7 +279,7 @@ const CustomerModal = ({
                   )}
                 </TouchableOpacity> */}
 
-                {/* ajksdfkjsdfg */}
+                {/* ajksdfksdadsfgdfgjsdfg */}
               </View>
             </View>
           </KeyboardAvoidingView>
