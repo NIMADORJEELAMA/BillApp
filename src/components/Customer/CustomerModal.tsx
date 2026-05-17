900fd8f533736a88ba0d400d74460e85e52a2e02
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-17 22:10:01 +0530

    update all12343534

--- src/components/Customer/CustomerModal.tsx
+++ src/components/Customer/CustomerModal.tsx
@@ -279,7 +279,7 @@ const CustomerModal = ({
                   )}
                 </TouchableOpacity> */}
 
-                {/* hjklhjk */}
+                {/* dfghdgdgfh */}
               </View>
             </View>
           </KeyboardAvoidingView>
