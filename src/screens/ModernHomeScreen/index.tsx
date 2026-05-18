5c8018041dfa835351c26141f1babcf55acb79fd
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-18 22:57:02 +0530

    upodateer123

--- src/screens/ModernHomeScreen/index.tsx
+++ src/screens/ModernHomeScreen/index.tsx
@@ -157,7 +157,7 @@ const ModernHomeScreen = () => {
         <TimelineChart />
       </Animated.View>
 
-      {/* 🟢 SECTION 3: SCROLL (TOP LAYER - SAME LEVEL AsdfgsdfgS HEADER) */}
+      {/* 🟢 SECTION 3: SCROLL (TOP LAYER - SAME LEVEL dfghdfghdfgh HEADER) */}
       <Animated.ScrollView
         ref={scrollRef}
         style={styles.scroll}
