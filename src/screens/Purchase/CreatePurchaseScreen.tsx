e9f1c951fafb71a40fbfe3d474d919f888eae0e5
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-19 22:56:13 +0530

    update nothing

--- src/screens/Purchase/CreatePurchaseScreen.tsx
+++ src/screens/Purchase/CreatePurchaseScreen.tsx
@@ -69,7 +69,7 @@ const CreatePurchaseScreen = ({navigation}) => {
       // ✅ Use data.items instead of data.data
       setSearchResults(data.items || []);
     } catch (error) {
-      console.log('Search Error:', error); // Add this temporarily so you can see if it fails
+      console.log('Search Error:', error); // Add this temporarily so you can see if it failsv sdfghsfd
     } finally {
       setIsSearching(false);
     }
