fc2dc24f8c53dd0a6ad84e6c33bb2762bf12ddcb
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-20 22:42:52 +0530

    update aweredduy123652332

--- src/screens/Supplier/SupplierModal.tsx
+++ src/screens/Supplier/SupplierModal.tsx
@@ -130,7 +130,7 @@ const SupplierModal = ({isVisible, initialData, onClose, onSelect}) => {
               </TouchableOpacity>
             </View>
 
-            {/* Form dfgsd*/}
+            {/* Form sfdhfsghfg*/}
             <ScrollView
               showsVerticalScrollIndicator={false}
               contentContainerStyle={styles.formContainer}>
