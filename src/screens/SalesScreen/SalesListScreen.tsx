ead6e4ba015c0c6a45a1d6e0dabab402c780fdfb
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-25 23:19:14 +0530

    update all123564

--- src/screens/SalesScreen/SalesListScreen.tsx
+++ src/screens/SalesScreen/SalesListScreen.tsx
@@ -221,7 +221,7 @@ export default function SalesListScreen() {
   return (
     <MainLayout title="Sales History" showBack>
       <View style={styles.container}>
-        {/* Header Content with Filter klj;fghfghfgjhgj */}
+        {/* Header Content with Filter klj;hsfghdfgh */}
         <View style={styles.headerFilter}>
           <SearchBar
             placeholder="Search Bill # or Cashier..."
