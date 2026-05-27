bac474762cf0417efbdd9eaa65ac18af6bc6c676
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-27 23:07:25 +0530

    updateui

--- src/screens/Purchase/CreatePurchaseScreen.tsx
+++ src/screens/Purchase/CreatePurchaseScreen.tsx
@@ -198,7 +198,7 @@ const CreatePurchaseScreen = ({navigation}) => {
           </TouchableOpacity>
         </View>
 
-        {/* Product Search & Cart */}
+        {/* Product Search & dfgsdfg */}
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Items</Text>
 
