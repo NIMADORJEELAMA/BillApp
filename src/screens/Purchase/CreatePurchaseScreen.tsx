4962d33a79c8a4d7257589ebf1fff2eba12492fa
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-05-27 23:09:34 +0530

    updateuiq3t

--- src/screens/Purchase/CreatePurchaseScreen.tsx
+++ src/screens/Purchase/CreatePurchaseScreen.tsx
@@ -198,7 +198,7 @@ const CreatePurchaseScreen = ({navigation}) => {
           </TouchableOpacity>
         </View>
 
-        {/* Product Search & dfgsdfg */}
+        {/* Product Search & asdfasdf */}
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Items</Text>
 
