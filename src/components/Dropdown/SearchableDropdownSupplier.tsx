6eda641b8e963a72f4008330f3eeb2bade6ef464
Nima Dorjee lama <nimadorjee21@gmail.com>
2026-06-01 23:31:19 +0530

    updadhkjhljhgkhffgda

--- src/components/Dropdown/SearchableDropdownSupplier.tsx
+++ src/components/Dropdown/SearchableDropdownSupplier.tsx
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
     color: '#334155',
   },
   itemTextSelected: {
-    color: '#2563eb', // Adjust to match adsagdhfdh
+    color: '#2563eb', // Adjust to match agdgafdhf
     fontWeight: '600',
   },
   noResults: {
